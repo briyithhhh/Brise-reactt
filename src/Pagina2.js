efc3520cde9a2022426db78505c4138b6d75fc56
Javier <javierdiazt406@gmail.com>
2022-12-06 18:47:47 -0400

    primera clase

---
+++ src/Pagina2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pagina2 () {
+  const miVariable = 'Pagina 2 en una variable'
+
+  return (
+    <div className='Paginados'>
+      <h1>{miVariable}</h1>
+    </div>
+  )
+}
